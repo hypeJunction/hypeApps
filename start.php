bbab7f446f7a0d0158d5409e60d59a03f31db46a
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-09-24 17:16:48 +0200

    chore(version): update verions

--- start.php
+++ start.php
@@ -7,9 +7,5 @@
  * @copyright Copyright (c) 2015, Ismayil Khayredinov
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2
  */
-try {
-	require_once __DIR__ . '/lib/autoloader.php';
-	hypeApps()->boot();
-} catch (Exception $ex) {
-	register_error($ex->getMessage());
-}
+require_once __DIR__ . '/autoloader.php';
+hypeApps()->boot();
