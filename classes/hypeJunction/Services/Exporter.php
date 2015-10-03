8fe96ee8989909cfdec6850f43a29da3cdbab68e
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-10-03 10:14:01 +0300

    feature(exporter): parameters are now a single array

--- classes/hypeJunction/Services/Exporter.php
+++ classes/hypeJunction/Services/Exporter.php
@@ -8,19 +8,20 @@ class Exporter {
 	 * Normalizes and exports data into a serializable array
 	 *
 	 * @param mixed $data      Object to export
-	 * @param array $fields    Fields to export
-	 * @param bool  $recursive Should entity owner and containers be exported in full?
+	 * @param array $params    Additionl params
+	 * @uses $params['fields'] An array of fields to export
+	 * @uses $params['recursive'] Export recursively
 	 * @return array
 	 */
-	public function export($data, $fields = array(), $recursive = false) {
+	public function export($data, array $params = array()) {
 		if (empty($data)) {
 			return $data;
 		}
-		
+
 		if ($data instanceof \ElggBatch) {
 			$return = array('items' => array());
 			foreach ($data as $v) {
-				$return['items'][] = $this->export($v, $fields, $recursive);
+				$return['items'][] = $this->export($v, $params);
 			}
 			$data = $return;
 		}
@@ -30,29 +31,26 @@ class Exporter {
 		} else if (is_array($data)) {
 			$return = array();
 			foreach ($data as $key => $v) {
-				$return[$key] = $this->export($v, $fields, $recursive);
+				$return[$key] = $this->export($v, $params);
 			}
 			return $return;
 		} else if ($data instanceof \hypeJunction\BatchResult) {
-			return $data->export($fields, $recursive);
+			return $data->export($params);
 		} else if ($data instanceof \ElggData || $data instanceof \ElggRiverItem) {
 
 			$array = is_callable(array($data, 'toObject')) ? (array) $data->toObject() : array();
 			$type = is_callable(array($data, 'getType')) ? $data->getType() : 'unknown';
 			$subtype = is_callable(array($data, 'getSubtype')) ? $data->getSubtype() : false;
 
-			$params = array(
-				'object' => $data,
-				'fields' => $fields,
-				'recursive' => $recursive,
-			);
+			$hook_params = $params;
+			$hook_params['object'] = $data;
 
-			$array = elgg_trigger_plugin_hook('to:array', $type, $params, $array);
+			$array = elgg_trigger_plugin_hook('to:array', $type, $hook_params, $array);
 			if ($subtype) {
-				$array = elgg_trigger_plugin_hook('to:array', "$type:$subtype", $params, $array);
+				$array = elgg_trigger_plugin_hook('to:array', "$type:$subtype", $hook_params, $array);
 			}
 
-			return $this->export($array, $fields, $recursive);
+			return $this->export($array, $params);
 		}
 
 		return $data;
